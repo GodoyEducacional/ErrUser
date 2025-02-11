ee44107bacab1c31d94c281b2b6b609ff4e9ea0f
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2025-02-11 20:44:48 -0300

    11-02-2025

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import bcrypt from "bcrypt"; // Criptografar senha
 import jwt from "jsonwebtoken"; // Criar e validar tokens JWT
 import dotenv from "dotenv"; // Ambiente com arquivo .env
 
+import User from "./models/usuarioModel.js";
+
 dotenv.config(); // Carrega as variaveis de ambiente do arq. .ENV
 
 const app = express();
@@ -15,13 +17,58 @@ app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem vindo a nossa API! " });
 });
 
+app.post("/auth/register", async (req, res) => {
+  const { name, email, password, confirmpassword } = req.body;
+
+  if (!name) {
+    return res.status(422).json({ msg: "O nome é obrigatório! " });
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email é obrigatório! " });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "A senha é obrigatória! " });
+  }
+
+  if (password != confirmpassword) {
+    return res
+      .status(422)
+      .json({ msg: "A senha e a confirmação precisam ser iguais!" });
+  }
+
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    return res.status(422).json({ msg: "Por favor, utilize outro E-mail!" });
+  }
+
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    name,
+    email,
+    passwordHash,
+  });
+
+  try {
+    await user.save(); // Salva o usuario no banco de Dados
+
+    res.status(201).json({ msg: "Usuário criado com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+});
+
 // Credenciais
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
 mongoose
   .connect(
-    `mongodb+srv://${dbUser}:${dbPassword}@clusterapi.h93mb.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAPI`
+    `mongodb+srv://${dbUser}:${dbPassword}@clusterapi.mmlf2.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAPI`
   )
   .then(() => {
     app.listen(3000);
