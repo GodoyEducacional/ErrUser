812d327571d778d353ef6154eaa3da489e401fbd
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2025-02-14 22:25:08 -0300

    Aula Token

--- app.js
+++ app.js
@@ -17,6 +17,37 @@ app.get("/", (req, res) => {
   res.status(200).json({ msg: "Bem vindo a nossa API! " });
 });
 
+// Rota privada
+app.get("/user/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findById(id, "-password");
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado!" });
+  }
+
+  res.status(200).json({ user });
+});
+
+// Midlewares (Checagem do Token)
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Extrai o JWT do cabeçalho
+
+  if (!token) return res.status(401).json({ msg: "Acesso Negado! " });
+
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret); // Verifica se o Token é realmente válido
+
+    next();
+  } catch (err) {
+    res.status(400).json({ msg: "O Token é inválido!" });
+  }
+}
+
 app.post("/auth/register", async (req, res) => {
   const { name, email, password, confirmpassword } = req.body;
 
@@ -57,6 +88,46 @@ app.post("/auth/register", async (req, res) => {
     await user.save(); // Salva o usuario no banco de Dados
 
     res.status(201).json({ msg: "Usuário criado com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+});
+
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(422).json({ msg: "O email deve ser preenchido!" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "A senha deve ser preenchida!" });
+  }
+
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não contém cadastro!" });
+  }
+
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Senha inválida!" });
+  }
+
+  // Criar um Env secret para evitar invasões
+  try {
+    const secret = process.env.SECRET;
+
+    const token = jwt.sign(
+      {
+        id: user._id, // Cria um token JWT contendo a ID do User
+      },
+      secret
+    );
+
+    res.status(200).json({ msg: "Autenticação realizada com sucesso!", token });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
